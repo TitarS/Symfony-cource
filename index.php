aec8068783ea26bbd18bcafa13e510d69cf15992
TitarS <titar.serge@gmail.com>
2018-03-05 16:32:38 +0200

    create project

--- index.php
+++ index.php
@@ -1,36 +1,67 @@
 <?php
-header('Content-type: text/html; charset=utf-8');
-require "phpQuery-onefile.php";
+// Start a Session
+//if( !session_id() ) @session_start();
 
+// Initialize Composer Autoload
+require_once 'vendor/autoload.php';
 
-/*ob_start();
-setcookie("cookiename", "cookiedata");*/
+use Respect\Validation\Validator as v;
+use Respect\Validation\Exceptions\ValidationException;
 
+/*$validator = v::key('username', v::stringType()->notEmpty())
+    ->key('email', v::email())
+    ->key('password', v::stringType()->notEmpty())
+    ->key('terms', v::trueVal())
+    ->keyValue('password_konfirmation', 'equals', 'password');
 
-function print_arr($arr) {
-	echo '<pre>' . print_r($arr, true) . '</pre>';
+try {
+    $validator->assert([
+            'username' => '',
+            'email' => '',
+            'password' => '',
+            'terms' => '',
+            'password_konfirmation' => '',
+    ]);
+} catch (ValidationException $exception) {
+    $exception->findMessages([
+        'username' => 'Введите имя',
+        'email' => 'Не верный email',
+        'password' => 'Введите пароль',
+        'terms' => 'Согласитесь с правилами',
+        'password_konfirmation' => 'Пароли не совпадают',
+    ]);
+
+    flash()->error($exception->getMessages());
 }
 
- 	$url = 'http://hitachi.tehno-lux.com.ua/products/r-v660puc3kpbe/';
-	$html = file_get_contents($url);
+echo flash();*/
 
-//xprint( $html, '$html');
-  	$doc = phpQuery::newDocument($file);
+$db = new \PDO('mysql:host=localhost;dbname=proj', 'root', '');
 
-  	$resultt = $doc->find('title_pr');
-  	echo "<pre>" . $resultt . "</pre>" . $resultt;
- print_arr($resultt);
+$auth = new \Delight\Auth\Auth($db);
 
-$i=0;
-  	foreach($doc->find('.articles-container .post-excerpt') as $article) {
-  		$article = pq($article);
-  		$img = $article->find('.img-cont img')->attr('src');
-  		print_arr($img);
-  		$i++;
-  		if ($i==3) break; 
-  	}
+try {
+    $userId = $auth->register('serhii.tytar@gmail.com', '123', 'titar', function ($selector, $token) {
 
-//ob_clean();
+        $url = 'http://www.proj/verify_email?selector=' . \urlencode($selector) . '&token=' . \urlencode($token);
 
+        echo $url; exit;
+    });
 
+    // we have signed up a new user with the ID `$userId`
+}
+catch (\Delight\Auth\UserAlreadyExistsException $e) {
+    flash()->error(['sorry, this dude already registered']);
+}
+catch (\Delight\Auth\TooManyRequestsException $e) {
+    flash()->error(['many request']);
+}
 ?>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<?= flash() ?>
+<!--
+<form action="">
+    <input type="text">
+    <input type="text">
+    <input type="text">
+</form>-->
